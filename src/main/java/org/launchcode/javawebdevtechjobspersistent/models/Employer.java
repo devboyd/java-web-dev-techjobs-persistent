fca9e0b9bd32ffc9c6c587b9a6612ff4604c8e4b
devboyd <devin4boyd@gmail.com>
2022-03-14 20:25:07 -0500

    add 2 repos, start on home controller

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,7 +1,29 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @OneToMany
+    @JoinColumn
+    List<Job> jobs = new ArrayList<>();
+
+    @NotNull
+    @Size(min = 2, max = 250)
     private String location;
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
